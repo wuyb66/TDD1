8cd0b982dbf9bbd764e0cc76ae8e5f6af1088b06
wuyb66 <wuyb66@foxmail.com>
2017-05-13 22:54:11 +0800

    Add one new test case test_can_start_a_list_and_retrieve_it_later using unittest.

--- test/functional_test.py
+++ test/functional_test.py
@@ -4,4 +4,4 @@ browser = webdriver.Firefox()
 
 browser.get("http://127.0.0.1:8000")
 
-assert 'Django' in browser.title
+assert 'To-Do' in browser.title, "Browser title was \"" + browser.title + '\"'
